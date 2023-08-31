9e096db7250522e683c8c64171ca33acf7922881
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-08-31 07:06:44 -0300

    chore: alteration LDV5 for phone

--- src/screens/RegisterMeals.tsx
+++ src/screens/RegisterMeals.tsx
@@ -81,7 +81,7 @@ export function RegisterMeals() {
         ? JSON.parse(searchCollaborator)
         : [];
 
-      if (colab.horaRefeicao > "05:59" && colab.horaRefeicao < "08:30") {
+      if (colab.horaRefeicao > "05:59" && colab.horaRefeicao < "07:30") {
         colab.tipoDaRefeicao = "CAFE";
         const collaboratorExistOrNot = previousCollaboratorData.find(
           (element) => element.matricula == colab.matricula
@@ -140,7 +140,7 @@ export function RegisterMeals() {
             setScanned(false);
           }, 2000);
         }
-      } else if (colab.horaRefeicao > "11:29" && colab.horaRefeicao < "14:00") {
+      } else if (colab.horaRefeicao > "11:29" && colab.horaRefeicao < "13:30") {
         colab.tipoDaRefeicao = "ALMOCO";
         const collaboratorExistOrNot = previousCollaboratorData.find(
           (element) => element.matricula == colab.matricula
@@ -198,7 +198,7 @@ export function RegisterMeals() {
             setScanned(false);
           }, 2000);
         }
-      } else if (colab.horaRefeicao > "17:59" && colab.horaRefeicao < "21:30") {
+      } else if (colab.horaRefeicao > "18:29" && colab.horaRefeicao < "20:30") {
         colab.tipoDaRefeicao = "JANTA";
         const collaboratorExistOrNot = previousCollaboratorData.find(
           (element) => element.matricula == colab.matricula
