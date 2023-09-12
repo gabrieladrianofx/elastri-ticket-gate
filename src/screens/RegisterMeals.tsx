865683466e93f3b2c33ae4e7d43c010ffc22ab95
Gabriel Adriano <gabrieladrianofx@gmail.com>
2023-09-12 11:05:20 -0300

    chore: alteration morro cruzeiro two

--- src/screens/RegisterMeals.tsx
+++ src/screens/RegisterMeals.tsx
@@ -81,7 +81,7 @@ export function RegisterMeals() {
         ? JSON.parse(searchCollaborator)
         : [];
 
-      if (colab.horaRefeicao > "04:59" && colab.horaRefeicao < "07:00") {
+      if (colab.horaRefeicao > "05:19" && colab.horaRefeicao < "08:00") {
         colab.tipoDaRefeicao = "CAFE";
         const collaboratorExistOrNot = previousCollaboratorData.find(
           (element) => element.matricula == colab.matricula
@@ -140,7 +140,7 @@ export function RegisterMeals() {
             setScanned(false);
           }, 2000);
         }
-      } else if (colab.horaRefeicao > "10:59" && colab.horaRefeicao < "13:30") {
+      } else if (colab.horaRefeicao > "10:59" && colab.horaRefeicao < "14:00") {
         colab.tipoDaRefeicao = "ALMOCO";
         const collaboratorExistOrNot = previousCollaboratorData.find(
           (element) => element.matricula == colab.matricula
@@ -198,7 +198,7 @@ export function RegisterMeals() {
             setScanned(false);
           }, 2000);
         }
-      } else if (colab.horaRefeicao > "17:59" && colab.horaRefeicao < "20:00") {
+      } else if (colab.horaRefeicao > "17:59" && colab.horaRefeicao < "21:30") {
         colab.tipoDaRefeicao = "JANTA";
         const collaboratorExistOrNot = previousCollaboratorData.find(
           (element) => element.matricula == colab.matricula
